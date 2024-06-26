346ca2bf580c81a03b772ce26cd04f72247ebd6d
Tymek399 <92734250+Tymek399@users.noreply.github.com>
2024-06-26 16:16:02 +0200

    Dodaj pliki projektów.

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">Prawda, jeśli zarządzane zasoby powinny być zlikwidowane; w przeciwnym razie fałsz.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta Windows Forms
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta - nie należy modyfikować
+        /// zawartości tej metody z edytorem kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelectFiles = new System.Windows.Forms.Button();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.btnSendToServer = new System.Windows.Forms.Button();
+            this.lstFiles = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnSelectFiles
+            // 
+            this.btnSelectFiles.Location = new System.Drawing.Point(12, 12);
+            this.btnSelectFiles.Name = "btnSelectFiles";
+            this.btnSelectFiles.Size = new System.Drawing.Size(150, 30);
+            this.btnSelectFiles.TabIndex = 0;
+            this.btnSelectFiles.Text = "Wybierz Pliki";
+            this.btnSelectFiles.UseVisualStyleBackColor = true;
+            this.btnSelectFiles.Click += new System.EventHandler(this.btnSelectFiles_Click);
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(12, 48);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(150, 30);
+            this.btnEncrypt.TabIndex = 1;
+            this.btnEncrypt.Text = "Szyfruj";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(12, 84);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(150, 30);
+            this.btnDecrypt.TabIndex = 2;
+            this.btnDecrypt.Text = "Deszyfruj";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // btnSendToServer
+            // 
+            this.btnSendToServer.Location = new System.Drawing.Point(12, 120);
+            this.btnSendToServer.Name = "btnSendToServer";
+            this.btnSendToServer.Size = new System.Drawing.Size(150, 30);
+            this.btnSendToServer.TabIndex = 3;
+            this.btnSendToServer.Text = "Wyślij na Serwer";
+            this.btnSendToServer.UseVisualStyleBackColor = true;
+            this.btnSendToServer.Click += new System.EventHandler(this.btnSendToServer_Click);
+            // 
+            // lstFiles
+            // 
+            this.lstFiles.FormattingEnabled = true;
+            this.lstFiles.ItemHeight = 16;
+            this.lstFiles.Location = new System.Drawing.Point(168, 12);
+            this.lstFiles.Name = "lstFiles";
+            this.lstFiles.Size = new System.Drawing.Size(400, 196);
+            this.lstFiles.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(580, 220);
+            this.Controls.Add(this.lstFiles);
+            this.Controls.Add(this.btnSendToServer);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.btnSelectFiles);
+            this.Name = "Form1";
+            this.Text = "Aplikacja do Szyfrowania Plików";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectFiles;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Button btnSendToServer;
+        private System.Windows.Forms.ListBox lstFiles;
+    }
+}
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        private List<string> selectedFiles = new List<string>();
+        private string encryptionKey = "twoj-klucz-szyfrowania";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectFiles_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Multiselect = true;
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    selectedFiles.AddRange(openFileDialog.FileNames);
+                    UpdateFileList();
+                }
+            }
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            Parallel.ForEach(selectedFiles, file => EncryptFile(file, encryptionKey));
+            MessageBox.Show("Szyfrowanie zakończone!");
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            Parallel.ForEach(selectedFiles, file => DecryptFile(file, encryptionKey));
+            MessageBox.Show("Deszyfrowanie zakończone!");
+        }
+
+        private void btnSendToServer_Click(object sender, EventArgs e)
+        {
+            Parallel.ForEach(selectedFiles, async file => await SendFileToServer(file));
+            MessageBox.Show("Wysyłanie zakończone!");
+        }
+
+        private void UpdateFileList()
+        {
+            lstFiles.Items.Clear();
+            foreach (var file in selectedFiles)
+                lstFiles.Items.Add(file);
+        }
+
+        private void EncryptFile(string filePath, string key)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = new byte[16];
+                using (FileStream fsInput = new FileStream(filePath, FileMode.Open))
+                using (FileStream fsEncrypted = new FileStream(filePath + ".enc", FileMode.Create))
+                using (CryptoStream cs = new CryptoStream(fsEncrypted, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                    fsInput.CopyTo(cs);
+            }
+        }
+
+        private void DecryptFile(string filePath, string key)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = new byte[16];
+                using (FileStream fsEncrypted = new FileStream(filePath, FileMode.Open))
+                using (FileStream fsDecrypted = new FileStream(filePath.Replace(".enc", ".dec"), FileMode.Create))
+                using (CryptoStream cs = new CryptoStream(fsEncrypted, aes.CreateDecryptor(), CryptoStreamMode.Read))
+                    cs.CopyTo(fsDecrypted);
+            }
+        }
+
+        private async Task SendFileToServer(string filePath)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var content = new MultipartFormDataContent();
+                content.Add(new StreamContent(File.OpenRead(filePath)), "file", Path.GetFileName(filePath));
+                await client.PostAsync("http://twój-serwer.com/upload", content);
+            }
+        }
+    }
+}
